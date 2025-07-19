a0fc94dbc91b60b846032d4747feb9fbdc416cf9
lamadaemon <i@lama.icu>
2025-07-19 19:34:00 +0800

    fix wrong bit mask writing method

--- src/esignature/esignature.c
+++ src/esignature/esignature.c
@@ -190,7 +190,7 @@ uint32_t zako_esign_verify(struct zako_esignature* esig, uint8_t* buff, size_t l
         off += sizeof(struct zako_der_certificate) + cert->len;
     }
 
-    result += zako_keychain_verify(&esig->key, &cstbl);
+    result |= zako_keychain_verify(&esig->key, &cstbl);
 
     OnNotFlag(flags, ZAKO_ESV_STRICT_MODE) {
         // todo tsa
@@ -198,8 +198,8 @@ uint32_t zako_esign_verify(struct zako_esignature* esig, uint8_t* buff, size_t l
 
 verify_integrity:
     pubkey = zako_parse_public_raw(esig->key.public_key);
-    if (zako_verify_buffer(pubkey, buff, len, esig->signature)) {
-        result += ZAKO_ESV_VERFICATION_FAILED;
+    if (zako_verify_buffer(pubkey, buff, len, esig->signature) != 1) {
+        result |= ZAKO_ESV_VERFICATION_FAILED;
     }
 
     EVP_PKEY_free(pubkey);
